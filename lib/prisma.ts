d5c787426c079deb3f8182419c319b2261e12a3a
MohamedAhmed <EngineerMohamed2016@gmail.com>
2026-05-12 04:23:03 +0300

    fix prisma setup for production

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaPg } from "@prisma/adapter-pg"; 
 import { PrismaClient } from "@prisma/client";
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient; 
-}; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
-export default prisma; 
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
